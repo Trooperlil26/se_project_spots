4861202f5bf6c6e205726010510950320c1fa78b
Khalil Keyes <keyeskhalil@gmail.com>
2025-04-27 19:18:34 -0400

    added the add profile modal to js

--- script/index.js
+++ script/index.js
@@ -2,10 +2,22 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
+const addProfileBtn = document.querySelector(".profile__add-btn");
+const addProfileModal = document.querySelector("#new-post-modal");
+const addProfileCloseBtn = addProfileModal.querySelector(".modal__close-btn");
+
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
+});
+
+addProfileBtn.addEventListener("click", function () {
+  addProfileModal.classList.add("modal_is-opened");
+});
+
+addProfileCloseBtn.addEventListener("click", function () {
+  addProfileModal.classList.remove("modal_is-opened");
 });
