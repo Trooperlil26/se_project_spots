352db703db4f2ae85d06721cb1603ce98094f1dc
Khalil Keyes <keyeskhalil@gmail.com>
2025-07-18 23:05:25 -0400

    feat: created validation.js, add setEventListeners/enableValidation function and iterated through inputList

--- script/validation.js
+++ script/validation.js
@@ -1,1 +1,25 @@
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
 
+  //TODO - handle initial states
+  //toggleButtonState(inputList, buttonElement);
+  //console.log(inputList);
+  //console.log(buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
