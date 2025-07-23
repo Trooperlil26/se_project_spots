4a906b95b652fff5b0292eb09fbbcdfab7d1cc2e
Khalil Keyes <keyeskhalil@gmail.com>
2025-07-22 21:42:40 -0400

    feat: add errorMsgEl and errorMsgID to html

--- script/validation.js
+++ script/validation.js
@@ -1,19 +1,24 @@
-const checkInputValidity = (formEl, inputElement) => {
-  console.log(inputElement.validity);
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgID = inputEl.id + "-error";
+  const errorMsgEl = document.querySelector("#" + errorMsgID);
+  errorMsgEl.textContent = errorMsg;
+};
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
 };
 
 const setEventListeners = (formEl) => {
   const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
   const buttonElement = formEl.querySelector(".modal__submit-btn");
 
-  //TODO - handle initial states
   //toggleButtonState(inputList, buttonElement);
-  //console.log(inputList);
-  //console.log(buttonElement);
 
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function () {
-      checkInputValidity(formEl, inputElement);
+  inputList.forEach((inputEl) => {
+    inputEl.addEventListener("input", function () {
+      checkInputValidity(formEl, inputEl);
       //toggleButtonState(inputList, buttonElement);
     });
   });
